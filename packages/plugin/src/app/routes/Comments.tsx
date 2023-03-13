757f09f856b4fd53ad7a10a5a325baaf468fe858
Abhijith v <abhijithababhijith@gmail.com>
2023-03-13 22:34:04 +0530

    added comment info section

--- packages/plugin/src/app/routes/Comments.tsx
+++ packages/plugin/src/app/routes/Comments.tsx
@@ -6,11 +6,38 @@ import MarkdownViewer from "../components/MarkdownViewer";
 import withHeader from "../hoc/header";
 import "../css/comments.css";
 
+const InfoSection = ({ issue }: { issue: Issue }) => {
+  return (
+    <div className="info-section">
+      {/* <h2>Comment Information</h2> */}
+      <div className="info-content">
+        <div className="assignee">
+          <img src={issue?.assignee?.avatar_url} alt="avatar" />
+          <p>Assignee: {issue?.assignee?.login}</p>
+        </div>
+        <p>Status: {issue?.state?.toUpperCase()}</p>
+        <div className="labels-section">
+          <h3>Labels:</h3>
+          <ul className="labels-list">
+            {issue?.labels?.map((label: any) => {
+              return (
+                <li key={label?.id} className="label">
+                  {label.name}
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+        <p></p>
+        {/* additional information */}
+      </div>
+    </div>
+  );
+};
 const Comments = () => {
   const [comments, setComments] = useState([]);
   const [isLoading, setLoading] = useState(false);
   const { token } = useGithub();
-  console.log(token);
   const location = useLocation();
   const fetchComments = async (commentUrl) => {
     setLoading(true);
@@ -38,23 +65,28 @@ const Comments = () => {
   }
   return (
     <div>
-      <ul className="comment-list">
-        {comments.map((comment) => (
-          <li key={comment.id} className="comment">
-            <div className="comment-header">
-              <div className="comment-author">{comment?.user?.login}</div>
-              <div className="comment-date">{comment.created_at}</div>
-            </div>
-            <div className="comment-body">
-              {comment?.body && (
-                <div>
-                  <MarkdownViewer>{comment?.body}</MarkdownViewer>
+      <div className="comments-container">
+        <div className="comments-section">
+          <ul className="comment-list">
+            {comments.map((comment) => (
+              <li key={comment.id} className="comment">
+                <div className="comment-header">
+                  <div className="comment-author">{comment?.user?.login}</div>
+                  <div className="comment-date">{comment.created_at}</div>
                 </div>
-              )}
-            </div>
-          </li>
-        ))}
-      </ul>
+                <div className="comment-body">
+                  {comment?.body && (
+                    <div>
+                      <MarkdownViewer>{comment?.body}</MarkdownViewer>
+                    </div>
+                  )}
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <InfoSection issue={location?.state} />
+      </div>
     </div>
   );
 };
