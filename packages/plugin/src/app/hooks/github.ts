32c91de7305bb376462553bc029e4a3033b88798
Abhijith v <abhijithababhijith@gmail.com>
2023-06-27 00:22:09 +0530

    logout function

--- packages/plugin/src/app/hooks/github.ts
+++ packages/plugin/src/app/hooks/github.ts
@@ -1,16 +1,23 @@
 import { useState, useEffect } from "react";
 
 const useGithub = () => {
-  const [token, setToken] = useState<any>({});
+  const [token, setToken] = useState<any>(undefined);
   useEffect(() => {
     parent.postMessage({ pluginMessage: { type: "get-github-token" } }, "*");
     window.onmessage = (event) => {
       try {
-        setToken(JSON.parse(event?.data?.pluginMessage?.token));
+        if (
+          event?.data?.pluginMessage?.token &&
+          event?.data?.pluginMessage?.token?.length > 0
+        ) {
+          setToken(JSON.parse(event?.data?.pluginMessage?.token));
+        } else {
+          setToken(undefined);
+        }
       } catch (error) {
         console.error(error);
       }
-    }
+    };
   }, []);
   return { token, setToken };
 };
